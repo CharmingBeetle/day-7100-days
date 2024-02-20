19ac6452477cc35ee08f916b84f0accdf36948cf
CharmingBeetle <104301944+CharmingBeetle@users.noreply.github.com>
2024-02-20 12:14:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("THE INCREDIBLE FAKE GOT FAN FINDER!")
+print()
+tv_show = input("what is the best TV show?: ")
+if tv_show == "got":
+    print("Yes that's why you're here. lets quiz you on that?...")
+    charac1 = input("Which character burned down the temple?: ")
+    if charac1 == "cersei":
+      print("yes nice guess, Let's do another.")
+    else:
+      print("wrong!")
+    charac2 = input("Which character killed the Night King?:")
+    if charac2 == "arya":
+      print("You were lucky! ok final question!")
+    else:
+      print("nope!")
+    charac3 = input("WHich character was had a vision of the Prince that was Promised?: ")
+    if charac3 == "danearys":
+          print("Well done! You are a true fan through and through!")
+    else:
+      print(" NOPE! You need to rewatch the entire series and read the books...TWICE! POSER!")
+else: 
+  print('meh')
